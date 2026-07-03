143d5742ee0aa503ab715a0024a2741fe9a87b13
arpit-dev-x <gupta.arpitkumar11112005@gmail.com>
2026-07-03 19:51:21 +0530

    URL for home page is created

--- TechHarbor/Portfolio/urls.py
+++ TechHarbor/Portfolio/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("portfolio/", include("Porfolio.urls"))
+    path("", views.home, name="home"),
 ]
